90be07d9ed08b7d6dcc57b140ec1a85921802662
Luca Fortini <Luca.Fortini@iit.it>
2023-01-10 18:58:09 +0100

    modified setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    scripts=['scripts/actor', 'scripts/bvh_skeleton'],
+    scripts=['scripts/actor', 'scripts/retarget'],
     # packages=['makehuman_gazebo_retargeting'],
     # package_dir={'': 'scripts'}
 )
